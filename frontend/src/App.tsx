4a6989439a1648c593465a3eb7619b89f94a33fb
ChloeHouvardas <chloehouvardas404@gmail.com>
2026-05-31 23:59:45 -0400

    Use Galaxy background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import Waves from './components/Waves'
+import Galaxy from './components/Galaxy'
 
 function App() {
   return (
     <main className="relative min-h-screen overflow-hidden bg-[rgb(18,18,20)]">
-      <Waves className="pointer-events-none" lineColor="black" />
-      <section id="center" className="relative z-10 flex min-h-screen items-center justify-center text-6xl text-white">
+      <div className="absolute inset-0">
+        <Galaxy density={1.5} glowIntensity={0.5} hueShift={240} saturation={0.8} />
+      </div>
+      <section id="center" className="pointer-events-none relative z-10 flex min-h-screen items-center justify-center font-['Aldrich',sans-serif] text-8xl text-zinc-400 md:text-9xl">
         Hi, my name is Chloe
       </section>
     </main>

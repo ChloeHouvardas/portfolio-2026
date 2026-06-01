516991a1f1f3a1e58ec387e37d4fcc4efbd2682b
ChloeHouvardas <chloehouvardas404@gmail.com>
2026-05-31 23:38:59 -0400

    Add Tailwind and Waves background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
+import Waves from './components/Waves'
+
 function App() {
   return (
-    <>
-      <section id="center" className="text-red-500">
-        Hello World
+    <main className="relative min-h-screen overflow-hidden bg-[rgb(18,18,20)]">
+      <Waves className="pointer-events-none" lineColor="black" />
+      <section id="center" className="relative z-10 flex min-h-screen items-center justify-center text-6xl text-white">
+        Hi, my name is Chloe
       </section>
-    </>
+    </main>
   )
 }
 
